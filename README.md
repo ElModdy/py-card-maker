b410629fe7ea4112c600e45938b5fff1da464172
ElModdy <modesto.davide@gmail.com>
2021-02-28 13:40:02 +0100

    final hoperfully

---
+++ README.md
@@ -0,0 +1,7 @@
+#Installation guide
+```
+pkg update
+pkg upgrade
+pkg install build-essential libjpeg-turbo nodejs git python pango
+pip install cairocffi protobuf pypdf pillow tinydb sqlite3
+```
--- page2img.py
+++ page2img.py
@@ -3,9 +3,13 @@ import papyrus_pb2
 import cairocffi
 import math
 
+from PIL import Image
+
 DPI = 200 # defined by cairocffi
-SQUID_PATH = "/data/data/com.steadfastinnovation.android.projectpapyrus/files/data/pages/"
+SQUID_PATH = "/data/data/com.steadfastinnovation.android.projectpapyrus/files/data/"
 ANKI_PATH = "/sdcard/AnkiDroid/collection.media/"
+DEBUG = True
+
 
 def cm_to_point(cm):
     return cm / 2.54 * DPI
@@ -25,7 +29,6 @@ def get_surface(items, height, width):
 
     for item in items:
         if item.type == papyrus_pb2.Item.Type.Value('Stroke'):
-            #print(item)
             context.save()
             # Translate to reference_point (stroke origin)
             context.translate(cm_to_point(item.stroke.reference_point.x), cm_to_point(item.stroke.reference_point.y))
@@ -39,22 +42,36 @@ def get_surface(items, height, width):
             context.set_line_cap(cairocffi.LINE_CAP_ROUND)
             context.move_to(0, 0)
 
+            if item.stroke.stroke_type == papyrus_pb2.Stroke.Highlight:
+                context.push_group()
+                context.set_source_rgba(argb[1], argb[2], argb[3], 1)
+                context.fill_preserve()
+                context.set_line_cap(cairocffi.LINE_CAP_SQUARE)
+
             for point in item.stroke.point:
                 context.line_to(cm_to_point(point.x), cm_to_point(point.y))
-                if point.HasField('pressure'):
+                if item.stroke.stroke_type == papyrus_pb2.Stroke.Highlight:
+                    context.set_line_width(width)
+                    # context.
+                elif point.HasField('pressure'):
                     context.set_line_width(width * point.pressure)
                 else:
                     context.set_line_width(width)
                 context.stroke()
                 context.move_to(cm_to_point(point.x), cm_to_point(point.y))
+            if item.stroke.stroke_type == papyrus_pb2.Stroke.Highlight:
+                context.pop_group_to_source()
+                context.paint_with_alpha(argb[0])
             context.restore()
         elif item.type == papyrus_pb2.Item.Type.Value('Shape') and item.shape.ellipse is not None:
+            width = item.shape.ellipse.weight * 0.3
 
             context.save()
             context.new_sub_path()
             context.translate(cm_to_point(item.shape.ellipse.center_x), cm_to_point(item.shape.ellipse.center_y))
             context.set_line_width(item.shape.ellipse.weight)
             argb = u32_to_4f(item.shape.ellipse.color)
+            context.set_line_width(width)
             context.set_source_rgba(argb[1], argb[2], argb[3], argb[0])
             context.scale(cm_to_point(item.shape.ellipse.radius_x), cm_to_point(item.shape.ellipse.radius_y))
             context.arc(0, 0, 1, (item.shape.ellipse.start_angle / 360) * 2 * math.pi,
@@ -63,7 +80,6 @@ def get_surface(items, height, width):
             context.stroke()
             context.restore()
         elif item.type == papyrus_pb2.Item.Type.Value('Text'):
-
             context.save()
             context.set_font_size(item.text.weight)
 
@@ -79,10 +95,53 @@ def get_surface(items, height, width):
                                                item.text.text, False)
             context.show_glyphs(glyphs)
             context.restore()
+
+        elif item.type == papyrus_pb2.Item.Type.Value('Image'):
+            if DEBUG:
+                print("Got an image!")
+                print(item.image.image_hash)
+
+            # Convert JPEG image to PNG
+            im = Image.open(SQUID_PATH + "imgs/" + item.image.image_hash)
+            im = im.crop((item.image.crop_bounds.left, item.image.crop_bounds.top, item.image.crop_bounds.right,
+                          item.image.crop_bounds.bottom))
+            im.save(SQUID_PATH + "imgs/" + item.image.image_hash + ".png", "PNG")
+            im.close()
+
+            matrix = cairocffi.Matrix()
+
+            scale_x = cm_to_point(item.image.bounds.right - item.image.bounds.left) / (
+                        item.image.crop_bounds.right - item.image.crop_bounds.left)
+            scale_y = cm_to_point(item.image.bounds.bottom - item.image.bounds.top) / (
+                        item.image.crop_bounds.bottom - item.image.crop_bounds.top)
+
+            if DEBUG:
+                print("Scale X: %d" % (1 / scale_x))
+                print("Scale Y: %d" % (1 / scale_y))
+                print("Translate: %d" % cm_to_point(item.image.bounds.left))
+            matrix.scale(1 / scale_x, 1 / scale_y)
+            matrix.translate(-cm_to_point(item.image.bounds.left), -cm_to_point(item.image.bounds.top))
+
+            im_surface = cairocffi.ImageSurface.create_from_png(
+                SQUID_PATH + "imgs/" + item.image.image_hash + ".png")
+            im_surface_pattern = cairocffi.SurfacePattern(im_surface)
+
+            im_surface_pattern.set_filter(cairocffi.FILTER_GOOD)
+            im_surface_pattern.set_matrix(matrix)
+
+            context.save()
+            context.set_source(im_surface_pattern)
+            context.rectangle(cm_to_point(item.image.bounds.left), cm_to_point(item.image.bounds.top),
+                              cm_to_point(item.image.bounds.right - item.image.bounds.left),
+                              cm_to_point(item.image.bounds.bottom - item.image.bounds.top))
+            context.fill()
+            context.restore()
         else:
             print(item)
             print("Item of type {} not supported".format(papyrus_pb2.Item.Type.Name(item.type)))
 
+    surface.flush()
+    surface.finish()
     return surface
 
 
@@ -114,7 +173,7 @@ def crop_surface(surface, bounds):
 
 
 def handle_page(page_name):
-    page_path = "{}{}.page".format(SQUID_PATH, page_name)
+    page_path = "{}pages/{}.page".format(SQUID_PATH, page_name)
 
     page = papyrus_pb2.Page()
     page.ParseFromString(open(page_path, 'rb').read())
@@ -130,11 +189,21 @@ def handle_page(page_name):
 
     btop = bdown = None
     for item in page.layer.item:
+        bounds = None
         if item.type == papyrus_pb2.Item.Type.Value('Stroke') and item.stroke.color != 4294961979:
-            if item.stroke.bounds.top > separator:
-                bdown = get_bigger(bdown, item.stroke.bounds)
-            else:
-                btop = get_bigger(btop, item.stroke.bounds)
+            bounds = item.stroke.bounds
+        elif item.type == papyrus_pb2.Item.Type.Value('Shape') and item.shape.type == 'Ellipse' :
+            bounds = item.shape.ellipse.bounds
+        elif item.type == papyrus_pb2.Item.Type.Value('Text'):
+            bounds = item.text.bounds
+        else:
+            print(item)
+
+            if bounds is not None:
+                if bounds.top > separator:
+                    bdown = get_bigger(bdown, bounds)
+                else:
+                    btop = get_bigger(btop, bounds)
 
     print("bTop")
     print(btop)
